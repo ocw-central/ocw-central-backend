3fe2c14e430da6fb24c7fc508e272bf10a715627
shibukazu <shibukazu@hotmail.co.jp>
2022-08-12 00:14:45 +0900

    fix: add chapterId

--- model/chapter.go
+++ model/chapter.go
@@ -2,9 +2,15 @@ package model
 
 import (
 	"time"
+
+	"github.com/oklog/ulid"
 )
 
+type ChapterId struct {
+	id ulid.ULID `desc:"ID"`
+}
 type Chapter struct {
+	chapterId     ChapterId `desc:"ID"`
 	startAt       time.Time `desc:"チャプター開始時間"`
 	topic         string    `desc:"チャプタータイトル"`
 	thumbnailLink string    `desc:"サムネイルリンク"`
