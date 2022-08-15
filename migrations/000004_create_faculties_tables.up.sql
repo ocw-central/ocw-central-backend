9ddeef6f4bb16e4b2031941f23bdb87ae617fb2b
Yasutaka Odo <yasutakaodo@gmail.com>
2022-08-15 23:49:26 +0900

    changed rank to job_title since rank is keyword in sql

--- migrations/000004_create_faculties_tables.up.sql
+++ migrations/000004_create_faculties_tables.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS faculties(
     id BINARY(16) NOT NULL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     department VARCHAR(100),
-    job_position VARCHAR(50),
+    job_title VARCHAR(50),
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
     
