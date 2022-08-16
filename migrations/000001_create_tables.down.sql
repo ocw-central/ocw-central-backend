98110268ac0555ab45ea63994f074b11280d0dfe
Yasutaka Odo <yasutakaodo@gmail.com>
2022-08-16 23:24:06 +0900

    removed blank lines

--- migrations/000001_create_tables.down.sql
+++ migrations/000001_create_tables.down.sql
@@ -1,26 +1,14 @@
 BEGIN;
-
 DROP TABLE IF EXISTS videos;
-
 DROP TABLE IF EXISTS subjects;
-
 DROP TABLE IF EXISTS syllabuses;
-
 DROP TABLE IF EXISTS chapters;
-
 DROP TABLE IF EXISTS faculties;
-
 DROP TABLE IF EXISTS pdfs;
-
 DROP TABLE IF EXISTS subpages;
-
 DROP TABLE IF EXISTS subject_related_subjects;
-
 DROP TABLE IF EXISTS subject_faculty_subjects;
-
 DROP TABLE IF EXISTS syllabus_faculties;
-
 DROP TABLE IF EXISTS video_faculties;
-
 COMMIT;
 
