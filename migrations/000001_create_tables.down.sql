ca77b5d89769423a79690222048fe858fbf23daf
Yasutaka Odo <yasutakaodo@gmail.com>
2022-08-16 23:14:39 +0900

    change names into plurals

--- migrations/000001_create_tables.down.sql
+++ migrations/000001_create_tables.down.sql
@@ -18,8 +18,8 @@ DROP TABLE IF EXISTS subject_related_subjects;
 
 DROP TABLE IF EXISTS subject_faculty_subjects;
 
-DROP TABLE IF EXISTS syllabus_faculty;
+DROP TABLE IF EXISTS syllabus_faculties;
 
-DROP TABLE IF EXISTS video_faculty;
+DROP TABLE IF EXISTS video_faculties;
 
 COMMIT;
