8e6467ac525d08e005d0ccb6588607786fa542d7
Yasutaka Odo <yasutakaodo@gmail.com>
2022-08-24 19:51:30 +0900

    fixed confict

--- model/video.go
+++ model/video.go
@@ -14,7 +14,7 @@ type Video struct {
 	ordering    int           `desc:"順番"`
 	link        string        `desc:"リンク"`
 	chapters    []Chapter     `desc:"チャプターs"`
-	faculty     string           `desc:"FacultyIds"`
+	faculty     string        `desc:"教員の氏名と所属職位"`
 	lecturedOn  time.Time     `desc:"講義日"`
 	videoLength time.Duration `desc:"動画の長さ"`
 	language    string        `desc:"使用言語"`
