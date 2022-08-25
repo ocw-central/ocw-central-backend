5307d752f51ff786e7582aa74d43aa38ec458cca
ooyamatakehisa <ooyamatakehisa@gmail.com>
2022-08-26 07:38:31 +0900

    fix custom models of graphql

--- graph/model/models.go
+++ graph/model/models.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"time"
+
 	"github.com/kafugen/ocwcentral/domain/usecase/dto"
 )
 
@@ -30,8 +32,26 @@ func (Subpage) IsNode()            {}
 func (this Subpage) GetID() string { return this.ID }
 
 type Syllabus struct {
-	dto.SyllabusDTO
-	Subpages []Subpage `json:"subpages"`
+	ID                string    `json:"id"`
+	Faculty           string    `json:"faculty"`
+	Language          string    `json:"language"`
+	SubjectNumbering  string    `json:"subjectNumbering"`
+	AcademicYear      int       `json:"academicYear"`
+	Semester          string    `json:"semester"`
+	NumCredit         int       `json:"numCredit"`
+	CourceFormat      string    `json:"courceFormat"`
+	AssignedGrade     string    `json:"assignedGrade"`
+	TargetedAudience  string    `json:"targetedAudience"`
+	CourceDayPeriod   string    `json:"courceDayPeriod"`
+	Outline           string    `json:"outline"`
+	Objective         string    `json:"objective"`
+	LessonPlan        string    `json:"lessonPlan"`
+	GradingMethod     string    `json:"gradingMethod"`
+	CourceRequirement string    `json:"courceRequirement"`
+	OutClassLearning  string    `json:"outClassLearning"`
+	Reference         string    `json:"reference"`
+	Remark            string    `json:"remark"`
+	Subpages          []Subpage `json:"subpages"`
 }
 
 func (Syllabus) IsNode()            {}
@@ -39,8 +59,14 @@ func (this Syllabus) GetID() string { return this.ID }
 
 // type Video dto.VideoDTO
 type Video struct {
-	dto.VideoDTO
-	Chapters []Chapter `json:"chapters"`
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Link        string    `json:"link"`
+	Chapters    []Chapter `json:"chapters"`
+	Faculty     string    `json:"faculty"`
+	LecturedOn  time.Time `json:"lecturedOn"`
+	VideoLength int       `json:"videoLength"`
+	Language    string    `json:"language"`
 }
 
 func (Video) IsNode()            {}
