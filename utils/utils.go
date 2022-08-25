daa6b8866c8cd47804b511de9cf2d838527c5b76
ooyamatakehisa <ooyamatakehisa@gmail.com>
2022-08-26 04:13:58 +0900

    add util func to create qustion mark string
    Co-authored-by: Yasutaka Odo <YasutakaOdo@gmail.com>

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,15 @@
 package utils
 
+import "strings"
+
 func ConvertNilToZeroValue[T any](value *T) T {
 	if value == nil {
 		var result T
 		return result
 	}
 	return *value
+}
+
+func GetQuestionMarkStrs(num int) string {
+	return strings.Join(strings.Split(strings.Repeat("?", num), ""), ",")
 }
