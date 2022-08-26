18bd5d816a709eba03be1a688c7ccce20abc280d
Yasutaka Odo <yasutakaodo@gmail.com>
2022-08-26 21:58:44 +0900

    half-way throught

---
+++ persistence/dto/syllabus.go
@@ -0,0 +1,24 @@
+package dto
+
+type SyllabusDTO struct {
+	Id                *[]byte `db:"id"`
+	Faculty           *string `db:"faculty"`
+	Language          *string `db:"language"`
+	SubjectNumbering  *string `db:"subject_numbering"`
+	AcademicYear      *string `db:"academic_year"`
+	Semester          *string `db:"semester"`
+	NumCredit         *int8   `db:"num_credit"`
+	CourseFormat      *string `db:"course_format"`
+	AssignedGrade     *string `db:"assigned_grade"`
+	TargettedAudience *string `db:"targetted_audience"`
+	CourseDayPeriod   *string `db:"course_day_period"`
+	Outline           *string `db:"outline"`
+	Objective         *string `db:"objective"`
+	LessonPlan        *string `db:"lesson_plan"`
+	GradingMethod     *string `db:"grading_method"`
+	CourseRequirement *string `db:"course_requirement"`
+	OutclassLearning  *string `db:"outclass_learning"`
+	Reference         *string `db:"reference"`
+	Remark            *string `db:"remark"`
+	subpageIds        *[]byte `db:"subpage_ids"`
+}
--- persistence/syllabus_repository_impl.go
+++ persistence/syllabus_repository_impl.go
@@ -1,9 +1,14 @@
 package persistence
 
 import (
-	"github.com/kafugen/ocwcentral/model"
+	"database/sql"
+	"fmt"
 
 	"github.com/jmoiron/sqlx"
+
+	"github.com/kafugen/ocwcentral/model"
+	"github.com/kafugen/ocwcentral/persistence/dto"
+	"github.com/kafugen/ocwcentral/utils"
 )
 
 type SyllabusRepositoryImpl struct {
@@ -15,5 +20,78 @@ func NewSyllabusRepositoryImpl(db *sqlx.DB) SyllabusRepositoryImpl {
 }
 
 func (sR SyllabusRepositoryImpl) GetById(id model.SyllabusId) (*model.Syllabus, error) {
+	syllabusIdBytes := id.ByteSlice()
+
+	syllabusDTO := dto.SyllabusDTO{}
+	syllabusSQL := `
+		SELECT
+			syllabuses.id,
+			subject_id,
+			faculty,
+			language,
+			subject_numbering,
+			academic_year,
+			semester,
+			num_credit,
+			course_format,
+			assigned_grade,
+			targetted_audience,
+			course_day_period,
+			outline,
+			objective,
+			lesson_plan,
+			grading_method,
+			course_requirement,
+			outclass_learning,
+			reference,
+			remark,
+			subpages.id AS subpages_id,
+			link
+		FROM syllabuses
+		LEFT JOIN subpages 
+		ON syllabuses.subjevt_id = subpages.subject_id
+		WHERE syllabuses.id = ?
+	`
+	var syllabusSubpageDTOs []dto.SyllabusSubpageDTO
+	if err := sR.db.Select(&syllabusSubpageDTOs, syllabusSQL, syllabusIdBytes); err != nil {
+		return nil, fmt.Errorf("failed on select to `syllabuses` table: %w", err)
+	}
+
+	if err := sR.db.Get(&syllabusDTO, syllabusSQL, syllabusIdBytes); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("specified syllabus not found: %w", err)
+		}
+		return nil, fmt.Errorf("failed on select to `syllabuses` table: %w", err)
+	}
+
+	syllabusId := *(*[16]byte)(*syllabusDTO.Id)
+
+	syllabus := model.NewSyllabusFromRepository(
+		model.SyllabusId(syllabusId),
+		utils.ConvertNilToZeroValue(syllabusDTO.Faculty),
+		utils.ConvertNilToZeroValue(syllabusDTO.Language),
+		utils.ConvertNilToZeroValue(syllabusDTO.SubjectNumbering),
+		utils.ConvertNilToZeroValue(syllabusDTO.AcademicYear),
+		utils.ConvertNilToZeroValue(syllabusDTO.Semester),
+		utils.ConvertNilToZeroValue(syllabusDTO.NumCredit),
+		utils.ConvertNilToZeroValue(syllabusDTO.CourseFormat),
+		utils.ConvertNilToZeroValue(syllabusDTO.AssignedGrade),
+		utils.ConvertNilToZeroValue(syllabusDTO.TargettedAudience),
+		utils.ConvertNilToZeroValue(syllabusDTO.CourseDayPeriod),
+		utils.ConvertNilToZeroValue(syllabusDTO.Outline),
+		utils.ConvertNilToZeroValue(syllabusDTO.Objective),
+		utils.ConvertNilToZeroValue(syllabusDTO.LessonPlan),
+		utils.ConvertNilToZeroValue(syllabusDTO.GradingMethod),
+		utils.ConvertNilToZeroValue(syllabusDTO.CourseRequirement),
+		utils.ConvertNilToZeroValue(syllabusDTO.OutclassLearning),
+		utils.ConvertNilToZeroValue(syllabusDTO.Reference),
+		utils.ConvertNilToZeroValue(syllabusDTO.Remark),
+		subpageIds,
+	)
+
+	return syllabus, nil
+}
+
+func (sR SubjectRepositoryImpl) GetByIds(id []model.SyllabusId) ([]*model.Syllabus, error) {
 	panic("not implemented")
 }
