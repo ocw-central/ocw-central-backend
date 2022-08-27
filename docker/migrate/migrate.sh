dfd454fa5643f5959fc26838891db76e5a7a6c46
shibukazu <shibukazu@hotmail.co.jp>
2022-08-28 03:19:02 +0900

    fix: fix migration source

--- docker/migrate/migrate.sh
+++ docker/migrate/migrate.sh
@@ -2,6 +2,6 @@
 
 /wait
 /migrate \
-  -source "github://kafugen/ocw-central-backend/migrations#main" \
+  -source "github://shibukazu:${ACCESS_TOKEN}@kafugen/ocw-central-backend/migrations#main" \
   -database "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DATABASE}" up \
   $@
