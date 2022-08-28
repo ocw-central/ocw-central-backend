c13f0b6aff7f1176d724996946e22b9908b73add
shibukazu <shibukazu@hotmail.co.jp>
2022-08-28 16:10:29 +0900

    fix: fix .env.template

--- docker/migrate/migrate.sh
+++ docker/migrate/migrate.sh
@@ -2,5 +2,6 @@
 
 /wait
 /migrate \
-  -source "github://${ACCESS_USER}:${ACCESS_TOKEN}@kafugen/ocw-central-backend/migrations#main" \
+  -source "github://${ACCESS_USER}:${ACCESS_TOKEN}@kafugen/ocw-central-backend/migrations#${MIGRATE_BRANCH}" \
   -database "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DATABASE}" up
+  
