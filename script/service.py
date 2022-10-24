50005d8204e67a6b81fa3c168faf9eab8a47fe6e
ooyamatakehisa <ooyamatakehisa@gmail.com>
2022-10-25 02:48:18 +0900

    add a func to print drive info

--- script/service.py
+++ script/service.py
@@ -5,3 +5,14 @@ from googleapiclient.discovery import build
 def get_service():
     credentials, _ = google.auth.default()
     return build('drive', 'v3', credentials=credentials)
+
+
+def get_about():
+    """
+    https://developers.google.com/drive/api/v3/reference/about
+    """
+    return get_service().about().get(fields="storageQuota").execute()
+
+
+if __name__ == "__main__":
+    print(get_about())
