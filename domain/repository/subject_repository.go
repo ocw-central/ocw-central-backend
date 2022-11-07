3f357e91edc223825c5929d8834d8d943ea76598
Takehisa Oyama <44559556+ooyamatakehisa@users.noreply.github.com>
2022-11-07 22:04:28 +0900

    add parameters to get random (#123)
    * add setting to select subjects with videos
    
    * add parameters to GetByRandom
    
    * add parameters to GetByRandom in different layers
    
    * fix sql
    
    * fix arguments
    
    * fix name inconsistency for mapper
    
    * add category params
    
    * update generated.go
    
    * remove unnecesarry + to concatinate strings
    
    * add keyString to enable caching
    
    * fix sql typo
    
    * reflect advice
    
    * small fixes
    
    Co-authored-by: Yasutaka Odo <YasutakaOdo@gmail.com>

--- domain/repository/subject_repository.go
+++ domain/repository/subject_repository.go
@@ -8,6 +8,6 @@ type SubjectRepository interface {
 	GetById(id model.SubjectId) (*model.Subject, error)
 	GetByIds(ids []model.SubjectId) ([]*model.Subject, error)
 	GetBySearchParameter(title string, faculty string, academicField string) ([]*model.Subject, error)
-	GetByRandom(numSubjects int) ([]*model.Subject, error)
+	GetByRandom(category string, series string, academicField string, numSubjects int) ([]*model.Subject, error)
 	GetByVideoIds(videoIds []model.VideoId) ([]*model.Subject, error)
 }
