2da905f3855620231c873a05390df200add05491
Jumon Nozaki <jumon.nozaki@gmail.com>
2022-11-16 23:17:44 +0900

    fix drop table order

--- migrations/000001_create_tables.down.sql
+++ migrations/000001_create_tables.down.sql
@@ -1,18 +1,17 @@
 BEGIN;
 
+DROP TABLE IF EXISTS subject_related_subjects;
+
+DROP TABLE IF EXISTS subpages;
+
+DROP TABLE IF EXISTS resources;
+
+DROP TABLE IF EXISTS chapters;
+
+DROP TABLE IF EXISTS syllabuses;
+
 DROP TABLE IF EXISTS videos;
 
 DROP TABLE IF EXISTS subjects;
 
-DROP TABLE IF EXISTS syllabuses;
-
-DROP TABLE IF EXISTS chapters;
-
-DROP TABLE IF EXISTS resources;
-
-DROP TABLE IF EXISTS subpages;
-
-DROP TABLE IF EXISTS subject_related_subjects;
-
 COMMIT;
-
