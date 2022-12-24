54494f1b68417b7f9900a033bef0386cec1360dd
shibukazu <shibukazu@hotmail.co.jp>
2022-12-24 17:47:11 +0900

    remove dedicated container for migration

--- docker/server/deploy.sh
+++ docker/server/deploy.sh
@@ -1,4 +1,24 @@
 #!/bin/sh
 
 /wait
+
+if [ $APP_ENV = "LOCAL" ]; then
+  /migrate \
+    -path $MIGRATIONS_DIR \
+    -database "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DATABASE}" up
+  /migrate \
+    -source "github://${ACCESS_USER}:${ACCESS_TOKEN}@kafugen/ocw-central-backend/migrations#${MIGRATE_BRANCH}" \
+    -database "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DATABASE}" up
+else
+  echo "[INFO migrate] Unknown APP_ENV: $APP_ENV"
+  exit 1
+fi
+
+if [ $? -eq 0 ]; then
+  echo "[INFO migrate] Migrations succeeded."
+else
+  echo "[INFO migrate] Migrations failed."
+  exit 1
+fi
+
 ./main
