1b432825514848c3eb586eb29ec7d3f6f9f312d5
shibukazu <shibukazu@hotmail.co.jp>
2022-12-24 17:51:49 +0900

    modify

--- docker/server/deploy.sh
+++ docker/server/deploy.sh
@@ -6,6 +6,7 @@ if [ $APP_ENV = "LOCAL" ]; then
   /migrate \
     -path $MIGRATIONS_DIR \
     -database "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DATABASE}" up
+elif [ $APP_ENV = "PROD" -o $APP_ENV = "DEV" ]; then
   /migrate \
     -source "github://${ACCESS_USER}:${ACCESS_TOKEN}@kafugen/ocw-central-backend/migrations#${MIGRATE_BRANCH}" \
     -database "mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@tcp(${MYSQL_HOST}:${MYSQL_PORT})/${MYSQL_DATABASE}" up
